76395a3bb044d32bc88a26e437ca7507d0db0edc
loma <57.loma@gmail.com>
2016-11-06 23:45:56 +0800

    add coverall integration

--- src/test/java/unit/tests/TestSimple.java
+++ src/test/java/unit/tests/TestSimple.java
@@ -5,6 +5,7 @@
  */
 package unit.tests;
 
+import com.openbravo.format.DoubleUtils;
 import junit.framework.TestCase;
 
 /**
@@ -15,5 +16,9 @@ public class TestSimple extends TestCase {
 	
 	public void testSimple() {
 		assertEquals(1,1);
+	}
+
+	public void testFixDecimals(){
+		assertEquals(5.4, DoubleUtils.fixDecimals(5.4));
 	}
 }
